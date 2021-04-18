26c0745038374790edafb690823a50c48df39b19
azalamin <azalamins@gmail.com>
2021-04-18 14:51:36 +0600

    Added a file

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// Falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truthy
+// true
+// " "
+// "0"
+// []
+// {}
+
+let name = {};
+
+if(name){
+   console.log('Condition is true')
+} else{
+   console.log('Condition is false')
+}
