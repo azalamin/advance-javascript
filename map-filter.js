56f4b126a313ab0a37d56fa1f6eeaee67917f526
azalamin <azalamins@gmail.com>
2021-04-19 01:57:01 +0600

    map filter cleared

---
+++ map-filter.js
@@ -0,0 +1,29 @@
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNumber = []
+// for (let i = 0; i < number.length; i++) {
+//    const element = number[i] * number[i];
+//    newNumber.push(element)
+   
+// }
+
+// let sum = 0;
+// const result = number.map(function(element){
+//    console.log(element)
+//    sum += element
+// })
+// console.log(sum)
+
+// const result = number.map(x => x * x);
+// console.log(result)
+
+// const result = number.filter(x => x % 2 === 1)
+// console.log(result)
+
+// const isThere = number.find(x => x > 5);
+// console.log(isThere)
+
+const res = number.map(function(x){
+   return x * x;
+})
+
+console.log(res)
